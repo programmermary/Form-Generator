ceed0dd4e3ce346e93a00cd3ded0e691d3e4618c
maryam_ihsan <maryamihsan601@gmail.com>
2025-06-22 12:04:49 +0300

    upload the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function App() {
+  console.log('this is dev')
+  return (
+   <div className='bg-black'>
+    <h1>this is dev </h1>
+   </div>
+  )
+}
+
+export default App
